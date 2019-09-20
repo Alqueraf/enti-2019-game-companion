68c3f843648a8dcb3271662b5b68ab72c4be6df9
Alex Queudot <alqueraf@gmail.com>
2019-09-20 12:40:58 +0200

    completed 20-09

--- app/src/main/java/com/alexqueudot/helloworld/MainActivity.kt
+++ app/src/main/java/com/alexqueudot/helloworld/MainActivity.kt
@@ -2,11 +2,21 @@ package com.alexqueudot.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        loginButton.setOnClickListener {
+            // Code here
+            Toast.makeText(this, "Login pressed", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
